090b4191d731fb371b1c1b21c77d3b7cd2ee1a03
jj15warrior <janek302010@gmail.com>
2021-10-10 22:16:52 +0200

    modified:   main.py

--- main.py
+++ main.py
@@ -10,13 +10,14 @@ head = {
     "Authorization": token
 }
 
-
 resp = requests.get("http://discord.com/api/users/@me", headers=head)
 if resp.status_code != 200:
     print("error. status code: ", resp.status_code)
     exit()
 user = json.loads(resp.content, object_hook=lambda d: SimpleNamespace(**d))
-print("logged in as: ", user.username+"#"+user.discriminator)
+print("logged in as: ", user.username + "#" + user.discriminator)
+
+lastcommand = 0
 
 while 1:
     command = input("discord-cli>>")
@@ -25,8 +26,7 @@ while 1:
         dms - list dms
         mass_dms - list group dms
         guilds - list servers
-        goto_dm - go to specific dm channel
-        goto_guild - go to specific server
+        goto - choose dm / server to move to
         channels - list channels in current server
         back - go back from server/dm
         history <x> - check last x messages in current channel
@@ -34,31 +34,51 @@ while 1:
         reply <x> - reply to message.
         unread_dm - show unread dms
         unread_guilds - show unread servers
+        help - show this help
         """)
-    if(command == "dms"):
+        lastcommand = 0
+    if (command == "dms"):
         dms_unparsed = requests.get("http://discord.com/api/users/@me/channels", headers=head)
         if dms_unparsed.status_code != 200:
             print("error code: ", dms_unparsed.status_code)
-        dms = json.loads (dms_unparsed.content, object_hook=lambda d: SimpleNamespace(**d))
+            exit()
+        dms = json.loads(dms_unparsed.content, object_hook=lambda d: SimpleNamespace(**d))
         index = 0
         for dm in dms:
             if dm.recipients != []:
-                if len(dm.recipients)==1:
-                    print(str(index)+": "+dm.recipients[0].username)
+                if len(dm.recipients) == 1:
+                    print(str(index) + ": " + dm.recipients[0].username)
                     index = index + 1
+        lastcommand = 1
+
     if command == "mass_dms":
         dms_unparsed = requests.get("http://discord.com/api/users/@me/channels", headers=head)
         if dms_unparsed.status_code != 200:
             print("error code: ", dms_unparsed.status_code)
+            exit()
         dms = json.loads(dms_unparsed.content, object_hook=lambda d: SimpleNamespace(**d))
         index = 1
         for dm in dms:
             if dm.recipients != []:
-                if len(dm.recipients)>1:
-                    print (index, end =": ")
+                if len(dm.recipients) > 1:
+                    print(index, end=": ")
                     index = index + 1
                     for recipient in dm.recipients:
-                        print(recipient.username, end ="; ")
+                        print(recipient.username, end="; ")
                     print()
+        lastcommand = 2
+
     if command == "guilds":
-        guilds_unparsed = requests
+        guilds_unparsed = requests.get("http://discord.com/api/users/@me/guilds", headers=head)
+        if guilds_unparsed.status_code != 200:
+            print("error code: ", guilds_unparsed.status_code)
+            exit()
+        guilds = json.loads(guilds_unparsed.content, object_hook=lambda d: SimpleNamespace(**d))
+        index = 0;
+        for guild in guilds:
+            index = index + 1
+            print(str(index) + ": " + guild.name)
+        lastcommand = 3
+
+    if command.startswith("goto"):
+        num =
