082bdae6ddecd6d3d3f6cfd839b58fb6460094a4
jj15warrior <janek302010@gmail.com>
2021-10-12 16:15:46 +0200

    modified:   main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ print("logged in as: ", user.username + "#" + user.discriminator)
 
 lastcommand = 0
 scope = 0
-
+channelscope_exists = False
 while 1:
     command = input("discord-cli>>")
     if (command == "help"):
@@ -36,6 +36,7 @@ while 1:
         unread_dm - show unread dms
         unread_guilds - show unread servers
         help - show this help
+        send_file - send file to current channel
         """)
         lastcommand = 0
     if (command == "dms"):
@@ -44,7 +45,7 @@ while 1:
             print("error code: ", dms_unparsed.status_code)
             exit()
         dms = json.loads(dms_unparsed.content, object_hook=lambda d: SimpleNamespace(**d))
-        index = 0
+        index = 1
 
         for dm in dms:
             if dm.recipients != []:
@@ -82,23 +83,50 @@ while 1:
             print(str(index) + ": " + guild.name)
         lastcommand = 3
 
+    if command == "channels":
+        lastcommand = 4
+        channels_unparsed = requests.get("https://discord.com/api/guilds/"+scopeuser.id+"/channels", headers=head)
+        if channels_unparsed.status_code != 200:
+            print("error code: ", channels_unparsed.status_code)
+            exit()
+        channels = json.loads(channels_unparsed.content, object_hook=lambda d: SimpleNamespace(**d))
+        index = 1
+        for channel in channels:
+            print(str(index)+": "+channel.name)
+            index = index + 1
+
+
     if command.startswith("send"):
         arg = command.split(" ", 1)[1:]
+        arg_joined = "".join(arg)
+        message = {
+            "content": str(arg_joined)
+        }
+        print(scopeuser.id, scopeuser)
+        if lastcommand == 1:
+            resp2 = requests.post("https://discord.com/api/v9/channels/"+scopeuser.id+"/messages", headers=head, data=message)
+        if lastcommand == 2:
+            resp2 = requests.post("https://discord.com/api/v9/channels/" + scopeuser.id + "/messages", headers=head, data=message)
+        if lastcommand == 3:
+            print ("choose channel first!")
 
-
-
+        if lastcommand == 4:
+            channel_msg_resp = requests.post("https://discord.com/api/channels/"+channelscope.id, headers=head, data=message)
+        print (channel_msg_resp.content)
     if command.startswith("goto"):
         num = command.split(" ", 1)
         scope = int(num[1])
-
         if lastcommand == 0:
             print("you have to list something first! try dms, mass_dms or guilds")
 
         if lastcommand == 1:
-            scopeuser = dms[scope]
+            scopeuser = dms[scope-1]
 
         if lastcommand == 2:
-            scopeuser = dms[scope]
+            scopeuser = dms[scope-1]
 
         if lastcommand == 3:
-            scopeuser = guilds[scope]
+            scopeuser = guilds[scope-1]
+        if lastcommand == 4:
+            channelscope_exists = True
+            channelscope = channels[scope-1]
